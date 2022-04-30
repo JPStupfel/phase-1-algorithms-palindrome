c5b9a56eddf1d40ea5872ef8dcf24238b506eff5
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-30 16:31:36 -0500

    commit tree 8ce8d360c78523c55120b1a808a2cf0dbefac5b6

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
 
 /* 
   Add your pseudocode here
 */
 
+//create a variable that will turn to false if the palandrom test fails
+let isPal = true
+
+//for i in wordArray, if its ever not true that word[i] === word[-i], change isPal to false
+
+for (i in word) {
+  if (word[i] !== word[word.length -1 - i]) {isPal = false}
+}
+
+return isPal
+
+}
 /*
   Add written explanation of your solution here
 */
